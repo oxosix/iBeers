7765a23dc4c1708234572751ab11298739e99c22
d90ares <daniloasantos@outlook.com>
2024-01-31 23:25:37 -0300

    add new functions and make improvments

--- internal/ibeers/service/service.go
+++ internal/ibeers/service/service.go
@@ -44,8 +44,8 @@ func (s *BeerService) GetBeerByID(ctx context.Context, ID int64) (*domain.Beer, 
 	return beer, nil
 }
 
-func (s *BeerService) SearchBeer(ctx context.Context, string) (*domain.Beer, error) {
-	beer, err := s.repository.Search()
+func (s *BeerService) SearchBeer(ctx context.Context, b string) (*domain.Beer, error) {
+	beer, err := s.repository.Search(b)
 	if err != nil {
 		return nil, err
 	}
@@ -53,24 +53,42 @@ func (s *BeerService) SearchBeer(ctx context.Context, string) (*domain.Beer, err
 	if beer == nil {
 		return nil, errors.New("Beer not found")
 	}
+	return beer, nil
 }
 
 func (s *BeerService) StoreBeer(ctx context.Context, beer *domain.Beer) error {
-	if beer, err := s.repository.Store(beer); err != nil {
-		return nil, err
+	if err := s.repository.Store(beer); err != nil {
+		return err
 	}
-	
+	return nil
 }
 
 func (s *BeerService) ValidateBeer(beer *domain.Beer) error {
 	if beer.Name == "" {
 		return errors.New("Name beer cannot be empty")
 	}
-	return nil
 
-	if beer.Style || beer.Type == "" {
-		return errors.New("Style and Type cannot be empty")
+	_, errStyle := s.GetBeerStyles(beer.Style.Name)
+	_, errType := s.GetBeerStyles(beer.Type.Name)
+
+	// Verifica se ocorreu algum erro em ambas as chamadas
+	if errStyle != nil || errType != nil {
+		return errors.New("Style or Type not found")
 	}
+
+	// Verifica se os nomes de estilo e tipo não são vazios
+	if beer.Style.Name == "" || beer.Type.Name == "" {
+		return errors.New("Style or type cannot be empty")
+	}
+
 	return nil
 
+}
+
+func (s *BeerService) GetBeerStyles(b string) ([]*domain.BeerStyle, error) {
+	return s.repository.GetBeerStyles(b)
+}
+
+func (s *BeerService) GetBeerTypes(b string) ([]*domain.BeerType, error) {
+	return s.repository.GetBeerTypes(b)
 }
