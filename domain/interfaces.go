389f022d4e67b661678cf46496e1cafccdf7dc72
d90ares <daniloasantos@outlook.com>
2024-02-08 20:45:34 -0300

    adjust interfaces

--- domain/interfaces.go
+++ domain/interfaces.go
@@ -3,14 +3,10 @@ package domain
 import "context"
 
 type Repository interface {
-	GetAll() ([]*Beer, error)
-	GetbyID(ctx context.Context) (*Beer, error)
-	Add(ctx context.Context) (*Beer, error)
-	// Adicionar outros métodos de repositório conforme necessário
+	GetAllBeers(ctx context.Context) ([]*Beer, error)
+	AddBeer(ctx context.Context, beer *Beer) (*Beer, error)
 }
 
 type UseCase interface {
-	GetAllBeers(ctx context.Context) ([]*Beer, error)
-	AddBeer(ctx context.Context, beer *Beer) (*Beer, error)
-	// Adicionar outros métodos de caso de uso conforme necessário
+	Repository
 }
