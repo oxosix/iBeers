a0d7179f0f20ccc95ee3854debd3edad426d07d6
d90ares <daniloasantos@outlook.com>
2024-02-07 22:01:31 -0300

    add new method

--- http/router/routes.go
+++ http/router/routes.go
@@ -8,5 +8,6 @@ import (
 
 func SetupRoutes(router *mux.Router, beerHandler *handler.BeerHandler) {
 	router.HandleFunc("/v1/beers", beerHandler.GetAllBeers).Methods("GET")
+	router.HandleFunc("/v1/beers", beerHandler.AddBeer).Methods("POST")
 	// Adicione outras rotas conforme necessário
 }
